278fc5cc67b2a6f62f127ac4ab50e7a279c3e8a9
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-11-06 17:36:22 +0700

    feature(chore): add google analytic tags

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
+import VueGtag from "vue-gtag"
 
 createApp(App)
     .use(router)
+    .use(VueGtag)
     .mount('#app')
