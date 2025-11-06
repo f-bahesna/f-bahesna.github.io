067662839e7bc77e9df63ab26b68c3d46e22df2f
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-11-06 17:47:00 +0700

    fix(chore): fix gtag API

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
-import {VueGtag} from "vue-gtag"
+import { createGtagPlugin } from "vue-gtag"
 
 createApp(App)
     .use(router)
-    .use(VueGtag, {
-        config: {id: "G-YH4KDREFBH"}
+    .use(createGtagPlugin, {
+        property: {
+            id: "G-YH4KDREFBH"
+        }
     })
     .mount('#app')
